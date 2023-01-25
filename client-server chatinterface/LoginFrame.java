9594ac93739b82d0e2dfc3db38860cf267d771e5
Radhikaajith22 <121189145+Radhikaajith22@users.noreply.github.com>
2023-01-25 20:21:14 +0530

    Add files via upload

---
+++ client-server chatinterface/LoginFrame.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package demoserver;
+
+/**
+ *
+ * @author ADMIN
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+ 
+public class LoginFrame extends JFrame implements ActionListener {
+    
+    Container container = getContentPane();
+    JLabel userLabel = new JLabel("USERNAME");
+    JLabel passwordLabel = new JLabel("PASSWORD");
+    JTextField userTextField = new JTextField();
+    JPasswordField passwordField = new JPasswordField();
+    JButton loginButton = new JButton("LOGIN");
+    JButton resetButton = new JButton("RESET");
+    JCheckBox showPassword = new JCheckBox("Show Password");
+    JFrame frame = new JFrame();
+    JButton adminButton = new JButton("admin?");
+ 
+    LoginFrame() {
+        setLayoutManager();
+        setLocationAndSize();
+        addComponentsToContainer();
+        addActionEvent();
+ 
+    }
+ 
+    public void setLayoutManager() {
+        container.setLayout(null);
+    }
+ 
+    public void setLocationAndSize() {
+        userLabel.setBounds(50, 150, 100, 30);
+        passwordLabel.setBounds(50, 220, 100, 30);
+        userTextField.setBounds(150, 150, 150, 30);
+        passwordField.setBounds(150, 220, 150, 30);
+        showPassword.setBounds(150, 250, 150, 30);
+        loginButton.setBounds(50, 300, 100, 30);
+        resetButton.setBounds(200, 300, 100, 30);
+        adminButton.setBounds(200,500,100,30);
+ 
+ 
+    }
+ 
+    public void addComponentsToContainer() {
+        container.add(userLabel);
+        container.add(passwordLabel);
+        container.add(userTextField);
+        container.add(passwordField);
+        container.add(showPassword);
+        container.add(loginButton);
+        container.add(resetButton);
+        container.add(adminButton);
+    }
+ 
+    public void addActionEvent() {
+        loginButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        showPassword.addActionListener(this);
+        adminButton.addActionListener(this);
+    }
+ 
+ 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Coding Part of LOGIN button
+        if (e.getSource() == loginButton) {
+            String userText;
+            String pwdText;
+            userText = userTextField.getText();
+            pwdText = passwordField.getText();
+            if (userText.equalsIgnoreCase("username") && pwdText.equalsIgnoreCase("12345")) {
+                
+                loginButton.addActionListener(new ActionListener() 
+{
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               LoginClient window=new LoginClient(); 
+                window.frame.setVisible(true);
+               
+               dispose();   
+            }
+          
+});
+                
+                
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+
+ 
+        }
+        
+        //Coding Part of RESET button
+        if (e.getSource() == resetButton) {
+            userTextField.setText("");
+            passwordField.setText("");
+        }
+       //Coding Part of showPassword JCheckBox
+        if (e.getSource() == showPassword) {
+            if (showPassword.isSelected()) {
+                passwordField.setEchoChar((char) 0);
+            } else {
+                passwordField.setEchoChar('*');
+            }
+ 
+ 
+        }
+        //coding for admin button
+        if (e.getSource() == adminButton)
+        { 
+           adminButton.addActionListener(new ActionListener() 
+{
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+                
+               
+               dispose();   
+            }
+          
+});
+            
+        }
+        
+    }
+ 
+}
+ 
+class Login {
+    public static void main(String[] a) {
+        LoginFrame frame = new LoginFrame();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 370, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+ 
+    }
+ 
+}
